fa65b36d6b748fdb9f387f0915d95e1d712a0af1
Humingbird <sonugaahmed@gmail.com>
2023-12-17 16:19:01 +0100

    basic async update

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,6 +6,9 @@ import random
 
 async def wait_random(max_delay: int = 10) -> float:
     """An asynchronous coroutine."""
-    wait_time = random.random() * max_delay
-    await asyncio.sleep(wait_time)
-    return wait_time
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
+
+
+print(asyncio.run(wait_random(2)))
